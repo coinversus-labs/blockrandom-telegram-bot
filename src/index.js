34299d63133a105d2741a7244377468fd153755a
giomalakmadze <giothenics@gmail.com>
2022-06-14 02:36:23 +0400

    add blockrandom converthash function, requires testing

--- src/index.js
+++ src/index.js
@@ -5,10 +5,36 @@ const TOKEN = process.env.TELEGRAM_TOKEN;
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
+const shortHash = (hash = "") => hash.substring(0, 4) + hash.substring(hash.length-4, hash.length); 
+
+function convertHash(hash){
+    blockRandomHash = shortHash(hash);
+    hashArray = blockRandomHash.toLowerCase().split("");
+    let sum = 0;
+    let i = 0;
+    hashRandomNumb = (function (){
+
+    hashArray.forEach(val => {
+    if(isNaN(val)){
+        sum = sum + (parseInt(alphabetArray.indexOf(val)) * Math.pow(p, i));
+    }else{
+        sum = sum + (parseInt(val) * Math.pow(p, i));
+    }
+
+    i = i + 1;
+
+    });
+    return sum;
+
+    })();
+}
+
 bot.on('message', (message) => {
 
     let chat_id = message.from.id;
 
-    bot.sendMessage(chat_id, "Response from CoinVS Server");
+    bot.sendMessage(chat_id, convertHash(message.text));
+
+    bot.sendMessage(chat_id, "Please send blockchain Hash to convert" + message.text);
 
 });
