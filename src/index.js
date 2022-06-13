d48a359fc927840c5c1b94d7eb5597df216e80fb
giomalakmadze <giothenics@gmail.com>
2022-06-14 02:09:29 +0400

    remove duplicate const

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
-const telegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = process.env.TELEGRAM_TOKEN;
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.on('message', (message) => {
+
+    let chat_id = message.from.id;
+
+    bot.sendMessage(chat_id, "Response from CoinVS Server");
+
+});
