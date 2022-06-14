2d199e5433077098c36b80fa027cfe74de31fe6e
giomalakmadze <giothenics@gmail.com>
2022-06-14 06:33:11 +0400

    add BlockRandom, fixes bot messages

--- src/index.js
+++ src/index.js
@@ -5,14 +5,26 @@ const TOKEN = process.env.TELEGRAM_TOKEN;
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-const shortHash = (hash = "") => hash.substring(0, 4) + hash.substring(hash.length-4, hash.length); 
+// Array of Alphabet starting with 1
+const alphabetArray = "0abcdefghijklmnopqrstuvwxyz".split("");
+let hashArray;
+let shortHash;
+let p = 31;
+let i = 0;
 
-function convertHash(hash){
-    blockRandomHash = shortHash(hash);
-    hashArray = blockRandomHash.toLowerCase().split("");
-    let sum = 0;
-    let i = 0;
-    hashRandomNumb = (function (){
+function convertToShortHash(hash){
+	
+	const shortHash = hash.substring(0, 4) + hash.substring(hash.length-4, hash.length);
+	return shortHash;
+	
+}
+
+function convertHashToNumber(hash){
+	hashArray = hash.toLowerCase().split("");
+	let blockRandomNumb = 0;
+	let sum = 0;
+	
+	blockRandomNumb = (function (){
 
     hashArray.forEach(val => {
     if(isNaN(val)){
@@ -24,17 +36,40 @@ function convertHash(hash){
     i = i + 1;
 
     });
+	i = 0;
     return sum;
 
     })();
+	
+	return blockRandomNumb;
+	
 }
 
 bot.on('message', (message) => {
+	
+	let chat_id = message.from.id;
+	
+	if(message.text === '/start'){
 
-    let chat_id = message.from.id;
+	bot.sendMessage(chat_id, `CryptRobot of @coinvs is at your service, 
+	send blockchain hash to convert to a number.
+	
+	Example: 
+	84vmWvKxebcnZL5JPnqkzPFVBaL4y5uGCRRJW2a9P4SL`);	
 
-    bot.sendMessage(chat_id, convertHash(message));
+	}else if(message.text.length < 8){
 
-    bot.sendMessage(chat_id, "Please send blockchain Hash to convert" + message);
+	bot.sendMessage(chat_id, `hash should contain at least 8 characters`);
+
+	}else{
+		
+	shortHash = convertToShortHash(message.text);
+	let blockRandomNumb = convertHashToNumber(shortHash);
+	
+    bot.sendMessage(chat_id, `Here is your hash converted to a number:
+	
+	${blockRandomNumb}`);
+	
+	}
 
 });
